7a43374fe26f036d3a96e03180a5edf2af7167de
AleksandrBirukov <hdnhdxv@gmail.com>
2023-10-23 18:59:49 +0400

    first commit

---
+++ src/main/java/com/example/pp_3_1_2/services/UserServices.java
@@ -0,0 +1,12 @@
+package com.example.pp_3_1_2.services;
+
+import com.example.pp_3_1_2.model.User;
+
+import java.util.List;
+
+public interface UserServices {
+    List<User> getAllUsers();
+    /*User getUser(int id);
+    void saveUser(User user);
+    void deleteUser(int id);*/
+}
---
+++ src/main/java/com/example/pp_3_1_2/services/UserServicesImpl.java
@@ -0,0 +1,36 @@
+package com.example.pp_3_1_2.services;
+
+import com.example.pp_3_1_2.dao.UserDao;
+import com.example.pp_3_1_2.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Component
+public class UserServicesImpl implements UserServices {
+    @Autowired
+    private UserDao userDao;
+
+    @Override
+    @Transactional
+    public List<User> getAllUsers() {
+        return userDao.getAllUsers();
+    }
+
+    /*@Override
+    public User getUser(int id) {
+        return userDao.getUser(id);
+    }
+
+    @Override
+    public void saveUser(User user) {
+        userDao.saveUser(user);
+    }
+
+    @Override
+    public void deleteUser(int id) {
+        userDao.deleteUser(id);
+    }*/
+}
